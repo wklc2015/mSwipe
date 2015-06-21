f69bed69a6ce96b36012c93dae0e5ed18d6cd9df
王坤 <202178695@qq.com>
2015-06-21 17:36:12 +0800

    第一个版本基本完整
    完善了插件的说明文档，规范了部分参数命名，修复了少量bug

--- src/mSwipe.js
+++ src/mSwipe.js
@@ -1,9 +1,26 @@
 /**
  * mSwipe.js plugin
+ * depend some tools function
+ * Utils.extend
+ * Utils.hasPerspective
+ * Utils.removeEvent
+ * Utils.addEvent
+ * Utils.hasPointer
+ * Utils.hasTouch
+ * Utils.prefixPointerEvent
+ * Utils.style.transitionDuration
+ * Utils.style.transform
+ * Utils.style.transformOrigin
+ * Utils.eventType
  * */
 BASE.namespace('BASE.COM.mSwipe');
 BASE.COM.mSwipe = (function() {
 	var Utils = BASE.COM.Utils,
+		eventName = {
+			start : ['mousedown', 'touchstart'][Number(Utils.hasTouch)],
+			move  : ['mousemove', 'touchmove'][Number(Utils.hasTouch)],
+			end   : ['mouseup', 'touchend'][Number(Utils.hasTouch)]
+		},
 		Init = function(container, options) {
 			return new Swipe(container, options);
 		};
@@ -14,13 +31,12 @@ BASE.COM.mSwipe = (function() {
 		if(!this.container) return;
 		this.options = {
 			selector               : '.mSwipe-items',
-			/*0:translation,1:scale,2:cover*/
 			animated               : 0,
-			/*effected when animated = 1*/
-			scaleTo                : 0.2,
+			zoomTo                 : 0.2,
+			zoomIng                : 0.1,
 			startSlide             : 0,
 			speed                  : 400,
-			distanceValue          : 100,
+			distanceValue          : 10,
 			direction              : 'y',
 			disableToNext          : false,
 			disableToPrev          : false,
@@ -28,162 +44,137 @@ BASE.COM.mSwipe = (function() {
 			disablePreventDefault  : true,
 			disableStopPropagation : true,
 			bindToWrapper          : true,
-			disableMouse           : true,
-			disablePointer         : true,
-			disableTouch           : false,
-			disableClass           : 'preventDefault',
 			translateZ             : true,
-			drag                   : true,
-			callback               : null
+			drag                   : true
 		};
 		Utils.extend(this.options, options);
 		this.slides = this.container.querySelectorAll(this.options.selector);
 		this.animated = this.options.animated;
-		this.scaleTo = this.options.scaleTo < 1 && this.options.scaleTo >= 0 ? this.options.scaleTo : 0.2;
+		this.zoomTo = this.options.zoomTo < 0.9 && this.options.zoomTo > 0.5 ? this.options.zoomTo : 0.9;
 		this.currIndex = this.options.startSlide;
 		this.speed = this.options.speed;
 		this.distanceValue = this.options.distanceValue;
 		this.direction = this.options.direction && this.options.direction == 'x' ? 'x' : 'y';
 		this.translateZ = Utils.hasPerspective && this.options.translateZ ? ' translateZ(0)' : '';
-		
+
 		this.disableToNext = this.options.disableToNext;
 		this.disableToPrev = this.options.disableToPrev;
 		this.disableToLoop = this.options.disableToLoop;
 		this.disablePreventDefault = this.options.disablePreventDefault;
 		this.disableStopPropagation = this.options.disableStopPropagation;
-		
+
 		this.startPosition = {};
 		this.moveDelta = {};
+		this._events = {};
 		this.isAnimating = false;
 		this.isMouseDown = false;
 		this.hasMoved = false;
 		this.moved = false;
 		this.distance = 0;
-		this.init();
+		//this.init();
+		this.target = this.options.bindToWrapper ? this.container : window;
 	}
 
 	Swipe.prototype = {
-		init               : function(remove) {
+		init               : function(event) {
 			this.initNodes(true);
-			this._initEvent();
+			this._resize();
+			if(!event){
+				this._start();
+				this.options.drag && (this._move());
+				this._end();
+			}
 		},
-		handleEvent        : function(e) {
-			var target = e.target;
-			var cls = target.className;
-			if(target == window || (cls.indexOf(this.options.disableClass) == -1)){
-				switch(e.type) {
-					case 'touchstart':
-					case 'pointerdown':
-					case 'MSPointerDown':
-					case 'mousedown':
-						this._start(e);
-						break;
-					case 'touchmove':
-					case 'pointermove':
-					case 'MSPointerMove':
-					case 'mousemove':
-						this.options.drag && (this._move(e));
-						break;
-					case 'touchend':
-					case 'pointerup':
-					case 'MSPointerUp':
-					case 'mouseup':
-					case 'touchcancel':
-					case 'pointercancel':
-					case 'MSPointerCancel':
-					case 'mousecancel':
-						this._end(e);
-						break;
-					case 'orientationchange':
-					case 'resize':
-						this._resize();
-						break;
-					case 'transitionend':
-					case 'webkitTransitionEnd':
-					case 'oTransitionEnd':
-					case 'MSTransitionEnd':
-						this._transitionEnd(e);
-						break;
+		on                 : function(type, fn) {
+			if(!this._events[type]){
+				this._events[type] = [];
+			}
+			this._events[type].push(fn);
+		},
+		off                : function(type, fn) {
+			if(this._events[type]){
+				var index = this._events[type].indexOf(fn);
+				if(index > -1){
+					this._events[type].splice(index, 1);
 				}
 			}
 		},
-		_initEvent         : function(remove) {
-			var eventType = remove ? Utils.removeEvent : Utils.addEvent,
-				target = this.options.bindToWrapper ? this.container : window;
-			eventType(window, 'orientationchange', this);
-			eventType(window, 'resize', this);
-			if(!this.options.disableMouse){
-				eventType(this.container, 'mousedown', this);
-				eventType(target, 'mousemove', this);
-				eventType(target, 'mousecancel', this);
-				eventType(target, 'mouseup', this);
-			}
-			if(Utils.hasPointer && !this.options.disablePointer){
-				eventType(this.container, Utils.prefixPointerEvent('pointerdown'), this);
-				eventType(target, Utils.prefixPointerEvent('pointermove'), this);
-				eventType(target, Utils.prefixPointerEvent('pointercancel'), this);
-				eventType(target, Utils.prefixPointerEvent('pointerup'), this);
-			}
-			if(Utils.hasTouch && !this.options.disableTouch){
-				eventType(this.container, 'touchstart', this);
-				eventType(target, 'touchmove', this);
-				eventType(target, 'touchcancel', this);
-				eventType(target, 'touchend', this);
-			}
-		},
-		_destroy           : function() {
-			this._initEvents(true);
-		},
-		_start             : function(e) {
-			if(this.disablePreventDefault){
-				e.preventDefault();
-			}
-			if(this.disableStopPropagation){
-				e.stopPropagation();
-			}
-			if(!this.isAnimating){
-				var touches = e.touches ? e.touches[0] : e;
-				this.startPosition = {
-					x : touches.pageX,
-					y : touches.pageY
-				};
-				this.moveDelta = {
-					x : 0,
-					y : 0
-				};
-				this.isAnimating = false;
-				this.isMouseDown = true;
-				this.moved = false;
-				this.disableMove = true;
-			}
-		},
-		_move              : function(e) {
-			if(this.disablePreventDefault){
-				e.preventDefault();
-			}
-			if(this.disableStopPropagation){
-				e.stopPropagation();
-			}
-			if(this.isMouseDown){
-				var touches = e.touches ? e.touches[0] : e;
-				this.moveDelta = {
-					x : touches.pageX - this.startPosition.x,
-					y : touches.pageY - this.startPosition.y
-				};
-				this.distance = this.moveDelta[this.direction];
-				if((this.disableToNext && this.moveDelta[this.direction] < 0) || (this.disableToPrev && this.moveDelta[this.direction] > 0)){
-					this.disableMove = true;
-				} else{
-					this.disableMove = false;
-					this.moved = true;
-					/*start move*/
-					this._effectMove(this.distance);
+		_execEvent         : function(type) {
+			var that = this;
+			if(this._events[type]){
+				for(var i = 0, l = this._events[type].length; i < l; i++){
+					this._events[type][i].apply(this, [].slice.call(arguments, 1));
 				}
 			}
-
+		},
+		_start             : function() {
+			var that = this;
+			this.container.addEventListener(eventName.start, function(e) {
+				if(Utils.eventType[e.type] != 1){
+					if(e.button !== 0){
+						return;
+					}
+				}
+				if(that.disablePreventDefault){
+					e.preventDefault();
+				}
+				if(that.disableStopPropagation){
+					e.stopPropagation();
+				}
+				if(!that.isAnimating){
+					var touches = e.touches ? e.touches[0] : e;
+					that.startPosition = {
+						x : touches.pageX,
+						y : touches.pageY
+					};
+					that.moveDelta = {
+						x : 0,
+						y : 0
+					};
+					that.isAnimating = false;
+					that.isMouseDown = true;
+					that.moved = false;
+					that.firstMoved = true;
+					that.disableMove = true;
+					that._execEvent('beforetouchStart', [that]);
+				}
+			}, false);
+		},
+		_move              : function() {
+			var that = this;
+			this.container.addEventListener(eventName.move, function(e) {
+				if(that.disablePreventDefault){
+					e.preventDefault();
+				}
+				if(that.disableStopPropagation){
+					e.stopPropagation();
+				}
+				if(that.isMouseDown){
+					var touches = e.touches ? e.touches[0] : e;
+					that.moveDelta = {
+						x : touches.pageX - that.startPosition.x,
+						y : touches.pageY - that.startPosition.y
+					};
+					that.distance = that.moveDelta[that.direction];
+					if((that.disableToNext && that.moveDelta[that.direction] < 0) || (that.disableToPrev && that.moveDelta[that.direction] > 0)){
+						that.disableMove = true;
+					} else{
+						if(that.firstMoved){
+							that._execEvent('touchStart', [that]);
+						}
+						that.disableMove = false;
+						that.moved = true;
+						that.firstMoved = false;
+						/*start move*/
+						that._effectMove(that.distance);
+						that._execEvent('touchMove', [that]);
+					}
+				}
+			}, false);
 		},
 		_effectMove        : function(distance) {
 			var activePageArray = [this.prevPage, this.currPage, this.nextPage];
+			var absDistance = Math.abs(distance), activePage, otherPage;
 			switch(this.animated) {
 				case 0:
 					for(var i = 0, len = 3; i < len; i++){
@@ -194,7 +185,6 @@ BASE.COM.mSwipe = (function() {
 					}
 					break;
 				case 1:
-					var absDistance = Math.abs(distance), activePage, otherPage;
 					this.setTransformOrigin(distance);
 					if(distance > 0){
 						activePage = this.prevPage;
@@ -206,14 +196,13 @@ BASE.COM.mSwipe = (function() {
 					this.currPage.style[Utils.style.transformOrigin] = this.origin;
 					if(activePage){
 						activePage.style[Utils.style.transform] = 'translate' + this.direction.toUpperCase() + '(' + distance + 'px)' + this.translateZ;
-						this.currPage.style[Utils.style.transform] = 'scale(' + (this.size - absDistance) / this.size + ')' + this.translateZ;
+						this.currPage.style[Utils.style.transform] = 'scale(' + (this.size - absDistance * this.options.zoomIng) / this.size + ')' + this.translateZ;
 					} else{
 						this.currPage.style[Utils.style.transform] = 'translate' + this.direction.toUpperCase() + '(' + distance / 2 + 'px)' + this.translateZ;
 					}
 					otherPage && (otherPage.style[Utils.style.transform] = 'translate' + this.direction.toUpperCase() + '(0px)' + this.translateZ);
 					break;
 				case 2:
-					var absDistance = Math.abs(distance), activePage, otherPage;
 					if(distance > 0){
 						activePage = this.prevPage;
 						otherPage = this.nextPage;
@@ -230,28 +219,32 @@ BASE.COM.mSwipe = (function() {
 					break;
 			}
 		},
-		_end               : function(e) {
+		_end               : function() {
 			var that = this;
-			if(this.isMouseDown){
-				this.isMouseDown = false;
-				if(this.options.drag){
-					(!this.disableMove && this.moved) && (this._effectEnd(this.distance));
-				} else{
-					var touches = e.touches ? e.changedTouches[0] : e;
-					this.moveDelta = {
-						x : touches.pageX - this.startPosition.x,
-						y : touches.pageY - this.startPosition.y
-					};
-					this.setTransformOrigin(this.moveDelta[this.direction]);
-					setTimeout(function() {
-						that._effectEnd(that.moveDelta[that.direction]);
-					}, 100)
+			this.container.addEventListener(eventName.end, function(e) {
+				if(that.isMouseDown){
+					that.isMouseDown = false;
+					if(that.options.drag){
+						(!that.disableMove && that.moved) && (that._effectEnd(that.distance));
+					} else{
+						var touches = e.touches ? e.changedTouches[0] : e;
+						that.moveDelta = {
+							x : touches.pageX - that.startPosition.x,
+							y : touches.pageY - that.startPosition.y
+						};
+						that.setTransformOrigin(that.moveDelta[that.direction]);
+						setTimeout(function() {
+							that._effectEnd(that.moveDelta[that.direction]);
+						}, 10)
+					}
+					that._execEvent('touchEnd', [that]);
 				}
-			}
+			}, false);
 		},
-		_effectEnd         : function(distance, btn) {
+		_effectEnd         : function(distance, btn, speed) {
 			this.isAnimating = true;
 			var value = this.distanceValue;
+			var speed = speed || this.speed;
 			var activePageArray = [this.prevPage, this.currPage, this.nextPage], size, activePage;
 			if(distance > value && this.prevPage){
 				//to prev
@@ -279,32 +272,32 @@ BASE.COM.mSwipe = (function() {
 				case 0:
 					for(var i = 0, len = 3; i < len; i++){
 						if(activePageArray[i]){
-							activePageArray[i].style[Utils.style.transitionDuration] = this.speed + 'ms';
+							activePageArray[i].style[Utils.style.transitionDuration] = speed + 'ms';
 							activePageArray[i].style[Utils.style.transform] = 'translate' + this.direction.toUpperCase() + '(' + size + 'px)' + this.translateZ;
 						}
 					}
 					break;
 				case 1:
-					this.currPage.style[Utils.style.transitionDuration] = this.speed + 'ms';
+					this.currPage.style[Utils.style.transitionDuration] = speed + 'ms';
 					this.currPage.style[Utils.style.transformOrigin] = this.origin;
 					if(activePage){
-						activePage.style[Utils.style.transitionDuration] = this.speed + 'ms';
+						activePage.style[Utils.style.transitionDuration] = speed + 'ms';
 						activePage.style[Utils.style.transform] = 'translate' + this.direction.toUpperCase() + '(' + size + 'px)' + this.translateZ;
 						if(size == 0){
 							this.currPage.style[Utils.style.transform] = 'scale(1) translate(0px, 0px)' + this.translateZ;
 						} else{
-							this.currPage.style[Utils.style.transform] = 'scale(' + this.scaleTo + ') translate(0px, 0px)' + this.translateZ;
+							this.currPage.style[Utils.style.transform] = 'scale(' + this.zoomTo + ') translate(0px, 0px)' + this.translateZ;
 						}
 					} else{
-						this.currPage.style[Utils.style.transform] = (this.scaleTo != 'null' ? 'scale(1)' : '') + ' translate(0px, 0px)';
+						this.currPage.style[Utils.style.transform] = (this.zoomTo != 'null' ? 'scale(1)' : '') + ' translate(0px, 0px)';
 					}
 					break;
 				case 2:
 					if(activePage){
-						activePage.style[Utils.style.transitionDuration] = this.speed + 'ms';
+						activePage.style[Utils.style.transitionDuration] = speed + 'ms';
 						activePage.style[Utils.style.transform] = 'translate' + this.direction.toUpperCase() + '(' + size + 'px)' + this.translateZ;
 					} else{
-						this.currPage.style[Utils.style.transitionDuration] = this.speed + 'ms';
+						this.currPage.style[Utils.style.transitionDuration] = speed + 'ms';
 						this.currPage.style[Utils.style.transform] = ' translate(0px, 0px)' + this.translateZ;
 					}
 					break;
@@ -313,19 +306,21 @@ BASE.COM.mSwipe = (function() {
 			var that = this;
 			setTimeout(function() {
 				that._transitionEnd(btn);
-			}, this.speed)
+			}, speed)
 		},
 		_resize            : function() {
-			this.initNodes();
+			var that = this;
+			window.addEventListener('resize', function() {
+				that.initNodes();
+			}, false);
 		},
 		_transitionEnd     : function(btn) {
 			this.isAnimating = false;
 			if(this.lastIndex != this.currIndex || btn){
-				if(this.options.callback && '[object Function]' === Object.prototype.toString.call(this.options.callback)){
-					this.options.callback(this);
-				}
+				this._execEvent('pageCallback', [this]);
 			}
-			this.initPosition(this.hasMoved);
+			this._execEvent('transitionEnd', [this]);
+			this.initPosition(true);
 		},
 		setTransformOrigin : function(distance) {
 			if(distance > 0){
@@ -347,24 +342,28 @@ BASE.COM.mSwipe = (function() {
 		},
 		initPosition       : function(hasMoved) {
 			if(hasMoved){
-				this.prevIndex = this.currIndex <= 0 ? null : this.currIndex - 1;
-				this.nextIndex = this.currIndex >= this.length - 1 ? null : this.currIndex + 1;
-				this.prevPage = this.slides[this.prevIndex];
-				this.currPage = this.slides[this.currIndex];
-				this.nextPage = this.slides[this.nextIndex];
+				if(this.disableToLoop){
+					this.prevIndex = this.currIndex <= 0 ? null : this.currIndex - 1;
+					this.nextIndex = this.currIndex >= this.length - 1 ? null : this.currIndex + 1;
+				} else{
+					this.prevIndex = this.currIndex <= 0 ? this.length - 1 : this.currIndex - 1;
+					this.nextIndex = this.currIndex >= this.length - 1 ? 0 : this.currIndex + 1;
+				}
 			}
-			this.lastIndex = this.currIndex;
 			var pos = this.direction === 'x' ? 'left' : 'top';
 			for(var i = 0; i < this.length; i++){
 				if(i == this.prevIndex){
 					this.slides[i].style[pos] = -this.size + 'px';
 					this.slides[i].setAttribute('data-slide', 'prev');
+					this.prevPage = this.slides[this.prevIndex];
 				} else if(i == this.currIndex){
 					this.slides[i].style[pos] = '0px';
 					this.slides[i].setAttribute('data-slide', 'curr');
+					this.currPage = this.slides[this.currIndex];
 				} else if(i == this.nextIndex){
 					this.slides[i].style[pos] = this.size + 'px';
 					this.slides[i].setAttribute('data-slide', 'next');
+					this.nextPage = this.slides[this.nextIndex];
 				} else{
 					this.slides[i].style[pos] = '0px';
 					this.slides[i].setAttribute('data-slide', 'none');
@@ -372,6 +371,7 @@ BASE.COM.mSwipe = (function() {
 				this.slides[i].style[Utils.style.transitionDuration] = '0s';
 				this.slides[i].style[Utils.style.transform] = 'none';
 			}
+			this.lastIndex = this.currIndex;
 		},
 		prev               : function() {
 			this.slideTo(this.prevIndex);
@@ -379,33 +379,36 @@ BASE.COM.mSwipe = (function() {
 		next               : function() {
 			this.slideTo(this.nextIndex);
 		},
-		slideTo            : function(num) {
+		slideTo            : function(num, speed) {
 			if(!this.isAnimating && typeof num == 'number'){
-				var self = this, symbol, origin;
+				var that = this, symbol;
 				if(num == this.currIndex){
 					this.isAnimating = false;
 				} else{
 					this.isAnimating = true;
 					if(num > this.currIndex){
 						this.nextIndex = num;
-						this.nextPage = this.slides[this.nextIndex];
+						this.prevIndex = null;
+						this.prevPage = null;
 						symbol = -200;
 						this.origin = this.direction === 'x' ? '0% 50%' : '50% 0%';
 					} else{
 						this.prevIndex = num;
-						this.prevPage = this.slides[this.prevIndex];
+						this.nextIndex = null;
+						this.nextPage = null;
 						symbol = 200;
 						this.origin = this.direction === 'x' ? '100% 50%' : '50% 100%';
 					}
-					this.currPage.style[Utils.style.transformOrigin] = origin;
+					this.currPage.style[Utils.style.transformOrigin] = this.origin;
 					this.initPosition();
+					this._execEvent('beforeSlide', [that, num]);
 					setTimeout(function() {
-						self._effectEnd(symbol);
-					}, 1)
+						that._effectEnd(symbol, true, speed);
+					}, 10)
 				}
 			}
 		}
-	}
+	};
 	return {
 		init : Init
 	}
